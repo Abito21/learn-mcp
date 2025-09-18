5295b4a5f13d4498523eec5753387dc69c293953
Abito21 <abidjuliant@gmail.com>
2025-09-18 21:13:59 +0700

    Feat : Using 2 mcp

--- consumer.py
+++ consumer.py
@@ -20,12 +20,20 @@ async def main():
           "FIRECRAWL_API_KEY": firecrawl_api_key
         }
     }
-    ) as server:
+    ) as server, MCPServerStdio(
+    params={
+        "command": "npx",
+        "args": ["-y", "firecrawl-mcp"],
+        "env": {
+          "FIRECRAWL_API_KEY": firecrawl_api_key
+        }
+    }
+    ) as server2 :
         agent = Agent(
             "Assistant",
             instructions="You are a helpful assistant",
             model="gpt-4.1",
-            mcp_servers=[server],
+            mcp_servers=[server, server2],
         )
         runner = await Runner.run(agent, input="Open Puppeteer, go to abidportfolio-web.vercel.app, and scrape the content!")
         print(runner.final_output)
